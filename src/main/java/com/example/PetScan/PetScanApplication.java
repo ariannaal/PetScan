9cd249e255565a3276f0296ca120daeca67a73c0
ariannaal <ariannaloreti25@gmail.com>
2024-09-30 12:22:22 +0200

    Initial commit

---
+++ src/main/java/com/example/PetScan/PetScanApplication.java
@@ -0,0 +1,13 @@
+package com.example.PetScan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetScanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetScanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PetScan/PetScanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PetScan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetScanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
