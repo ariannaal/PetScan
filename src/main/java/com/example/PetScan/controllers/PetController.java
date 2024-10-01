409cb97d5ce10b29d1fea3023e65c81099960bee
ariannaal <ariannaloreti25@gmail.com>
2024-10-01 12:21:10 +0200

    adds cloudinary to upload pictures for animals

--- src/main/java/com/example/PetScan/controllers/PetController.java
+++ src/main/java/com/example/PetScan/controllers/PetController.java
@@ -4,6 +4,7 @@ import com.example.PetScan.entities.Owner;
 import com.example.PetScan.entities.Pet;
 import com.example.PetScan.exceptions.BadRequestEx;
 import com.example.PetScan.exceptions.NotFoundEx;
+import com.example.PetScan.exceptions.UnauthorizedEx;
 import com.example.PetScan.payloads.NewPetDTO;
 import com.example.PetScan.repositories.OwnerRepository;
 import com.example.PetScan.repositories.PetRepository;
@@ -13,6 +14,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/pets")
@@ -27,7 +33,7 @@ public class PetController {
     @Autowired
     private PetRepository petRepository;
 
-
+    // POST http://localhost:3001/pets
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Pet savePet(@RequestBody @Validated NewPetDTO body, BindingResult validation) {
@@ -37,6 +43,29 @@ public class PetController {
         }
 
         return petService.savePet(body);
-
     }
+
+    // GET http://localhost:3001/pets / {id}
+    @GetMapping("/{id}")
+    private Pet getSinglePet(@PathVariable UUID id){
+        return petService.findById(id);
+    }
+
+    // **GET http://localhost:3001/pets
+    @GetMapping
+    public List<Pet> getAllPets() {
+        return petService.petsList();
+    }
+
+
+    // POST http://localhost:3001/pets/{id}/picture
+    @PostMapping("/{id}/picture")
+    public void uploadPicture(@PathVariable UUID id, @RequestParam("picture") MultipartFile image ) throws IOException {
+        Pet pet = petRepository.findById(id)
+                .orElseThrow(() -> new NotFoundEx("Animale non trovato per ID: " + id));
+
+        petService.uploadPicture(id, image);
+    }
+
+
 }
--- src/main/java/com/example/PetScan/services/PetService.java
+++ src/main/java/com/example/PetScan/services/PetService.java
@@ -1,5 +1,7 @@
 package com.example.PetScan.services;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import com.example.PetScan.entities.Owner;
 import com.example.PetScan.entities.Pet;
 import com.example.PetScan.exceptions.NotFoundEx;
@@ -8,6 +10,12 @@ import com.example.PetScan.repositories.OwnerRepository;
 import com.example.PetScan.repositories.PetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 @Service
 public class PetService {
@@ -17,6 +25,9 @@ public class PetService {
 
     @Autowired
     private OwnerRepository ownerRepository;
+
+    @Autowired
+    private Cloudinary cloudinary;
 
     public Pet savePet(NewPetDTO body) {
 
@@ -34,10 +45,41 @@ public class PetService {
         );
 
         Pet savedPet = petRepository.save(newPet);
-
         System.out.println("L'animale " + savedPet.getName() + " e' stato salvato con successo!");
-
         return savedPet;
+    }
+
+    public List<Pet> petsList() {
+        return petRepository.findAll();
+    }
+
+    public Pet findById(UUID id) {
+        return petRepository.findById(id).orElseThrow(() -> new NotFoundEx(String.valueOf(id)));
+    }
+
+    public void findByIdAndDelete(UUID id) {
+        Pet found = this.findById(id);
+        petRepository.delete(found);
+    }
+
+    public Pet findByIdAndUpdate(UUID id, Pet body) {
+        Pet found = this.findById(id);
+        found.setAge(body.getAge());
+        found.setOwner(body.getOwner());
+        found.setPicture(body.getPicture());
+        return petRepository.save(found);
+    }
+
+    public void uploadPicture(UUID id, MultipartFile image) throws IOException {
+        Pet pet = petRepository.findById(id)
+                .orElseThrow(() -> new NotFoundEx("Animale con id " + id + " non trovato."));
+
+        Map<String, Object> uploadResult = cloudinary.uploader().upload(image.getBytes(), ObjectUtils.emptyMap());
+        String picture = uploadResult.get("url").toString();
+        System.out.println("URL: " + picture);
+
+        pet.setPicture(picture);
+        petRepository.save(pet);
     }
 
 
