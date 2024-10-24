a5aaa2180666589074ab6b568ee303efb6096e15
ariannaal <ariannaloreti25@gmail.com>
2024-10-24 23:23:09 +0200

    fix cors

--- src/main/java/com/example/PetScan/PetScanApplication.java
+++ src/main/java/com/example/PetScan/PetScanApplication.java
@@ -12,4 +12,18 @@ public class PetScanApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PetScanApplication.class, args);
 	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("https://petscan.netlify.app", "http://localhost:5173")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(true);  // Aggiungi questo se usi cookie o token
+			}
+		};
+	}
 }
