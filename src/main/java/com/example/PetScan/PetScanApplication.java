f2a3206317da06ae10c1f86b11a63de98ac83670
ariannaal <ariannaloreti25@gmail.com>
2024-10-25 11:12:46 +0200

    fix cors

--- src/main/java/com/example/PetScan/PetScanApplication.java
+++ src/main/java/com/example/PetScan/PetScanApplication.java
@@ -19,15 +19,15 @@ public class PetScanApplication {
 		SpringApplication.run(PetScanApplication.class, args);
 	}
 
-	@Bean
-	public UrlBasedCorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("https://petscan.netlify.app", "http://localhost:5173"));
-		configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source;
-	}
+//	@Bean
+//	public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+//		CorsConfiguration configuration = new CorsConfiguration();
+//		configuration.setAllowedOrigins(Arrays.asList("https://petscan.netlify.app", "http://localhost:5173"));
+//		configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//		source.registerCorsConfiguration("/**", configuration);
+//		return source;
+//	}
 
 	@Bean
 	SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
